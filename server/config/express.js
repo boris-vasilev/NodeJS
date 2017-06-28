77c4450e5ee3b8d8aae806981107a7c8e17d7624
boris <borisvasilev395@gmail.com>
2017-06-29 00:19:27 +0300

    Added passport

--- server/config/express.js
+++ server/config/express.js
@@ -2,11 +2,22 @@
  * Created by boris on 6/28/2017.
  */
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+
 const handlebars = require('express-handlebars')
 module.exports=(app)=>{
     app.engine('handlebars',handlebars({
         defaultLayout:'main'
     }))
     app.set('view engine','handlebars')
+    app.use(cookieParser())
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(session({ secret: 'ywgyGTYFsd2*fy82734*&*#$Jdausdh',
+        resave: false, saveUninitialized: false }))
+    app.use(passport.initialize())
+    app.use(passport.session())
     app.use('/public',express.static('public'))
 }
